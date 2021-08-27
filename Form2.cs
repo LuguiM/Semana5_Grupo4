5c88094237bc46fdfec89e819714afd1a60f0b15
Kerin Meléndez <88210765+iiLynn@users.noreply.github.com>
2021-08-27 17:21:47 -0600

    Update Form2.cs

--- Form2.cs
+++ Form2.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Reflection;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+//hi
 namespace Semana5_Grupo4_controles_de_formularios
 {
     partial class Form2 : Form
