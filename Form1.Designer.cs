d694bb3fdcb6328528da07b0c1e74aff41639be2
JOSÉ LUIS MUÑOZ PADILLA <USIS043721@ugb.edu.sv>
2021-08-27 16:38:24 -0600

    ok

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -146,7 +146,7 @@ namespace Semana5_Grupo4_controles_de_formularios
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(800, 395);
             this.Controls.Add(this.button6);
             this.Controls.Add(this.button5);
             this.Controls.Add(this.button4);
