d8b58e1ad36cbca743caeba8b5edc93207e1020a
kerin melendez <valencia.mldz13@gmail.com>
2021-08-29 16:27:57 -0600

    1

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -48,9 +48,10 @@ namespace Semana5_Grupo4_controles_de_formularios
             // 
             this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
             this.button1.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(28, 77);
+            this.button1.Location = new System.Drawing.Point(51, 142);
+            this.button1.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(192, 71);
+            this.button1.Size = new System.Drawing.Size(352, 131);
             this.button1.TabIndex = 0;
             this.button1.Text = "ACEPTAR";
             this.button1.UseVisualStyleBackColor = false;
@@ -60,9 +61,10 @@ namespace Semana5_Grupo4_controles_de_formularios
             // 
             this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
             this.button2.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.Location = new System.Drawing.Point(251, 77);
+            this.button2.Location = new System.Drawing.Point(460, 142);
+            this.button2.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(193, 71);
+            this.button2.Size = new System.Drawing.Size(354, 131);
             this.button2.TabIndex = 1;
             this.button2.Text = "ACEPTAR/CANCELAR";
             this.button2.UseVisualStyleBackColor = false;
@@ -71,9 +73,10 @@ namespace Semana5_Grupo4_controles_de_formularios
             // 
             this.button3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
             this.button3.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button3.Location = new System.Drawing.Point(472, 77);
+            this.button3.Location = new System.Drawing.Point(865, 142);
+            this.button3.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(194, 71);
+            this.button3.Size = new System.Drawing.Size(356, 131);
             this.button3.TabIndex = 2;
             this.button3.Text = "Si/No";
             this.button3.UseVisualStyleBackColor = false;
@@ -83,9 +86,10 @@ namespace Semana5_Grupo4_controles_de_formularios
             // 
             this.button4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
             this.button4.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button4.Location = new System.Drawing.Point(28, 200);
+            this.button4.Location = new System.Drawing.Point(51, 369);
+            this.button4.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(192, 71);
+            this.button4.Size = new System.Drawing.Size(352, 131);
             this.button4.TabIndex = 3;
             this.button4.Text = "ACEPTAR";
             this.button4.UseVisualStyleBackColor = false;
@@ -94,9 +98,10 @@ namespace Semana5_Grupo4_controles_de_formularios
             // 
             this.button5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
             this.button5.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button5.Location = new System.Drawing.Point(251, 200);
+            this.button5.Location = new System.Drawing.Point(460, 369);
+            this.button5.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(193, 71);
+            this.button5.Size = new System.Drawing.Size(354, 131);
             this.button5.TabIndex = 4;
             this.button5.Text = "ACEPTAR";
             this.button5.UseVisualStyleBackColor = false;
@@ -105,15 +110,17 @@ namespace Semana5_Grupo4_controles_de_formularios
             // 
             this.button6.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
             this.button6.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button6.Location = new System.Drawing.Point(472, 200);
+            this.button6.Location = new System.Drawing.Point(865, 369);
+            this.button6.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(194, 71);
+            this.button6.Size = new System.Drawing.Size(356, 131);
             this.button6.TabIndex = 5;
             this.button6.Text = "ACEPTAR";
             this.button6.UseVisualStyleBackColor = false;
             // 
             // menuStrip1
             // 
+            this.menuStrip1.GripMargin = new System.Windows.Forms.Padding(2, 2, 0, 2);
             this.menuStrip1.ImageScalingSize = new System.Drawing.Size(28, 28);
             this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.menu1ToolStripMenuItem,
@@ -123,47 +130,48 @@ namespace Semana5_Grupo4_controles_de_formularios
             this.menu5ToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(921, 24);
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(11, 4, 0, 4);
+            this.menuStrip1.Size = new System.Drawing.Size(1689, 42);
             this.menuStrip1.TabIndex = 6;
             this.menuStrip1.Text = "menuStrip1";
             // 
             // menu1ToolStripMenuItem
             // 
             this.menu1ToolStripMenuItem.Name = "menu1ToolStripMenuItem";
-            this.menu1ToolStripMenuItem.Size = new System.Drawing.Size(56, 20);
-            this.menu1ToolStripMenuItem.Text = "Menu1";
+            this.menu1ToolStripMenuItem.Size = new System.Drawing.Size(116, 34);
+            this.menu1ToolStripMenuItem.Text = "Archivos ";
             // 
             // menu2ToolStripMenuItem
             // 
             this.menu2ToolStripMenuItem.Name = "menu2ToolStripMenuItem";
-            this.menu2ToolStripMenuItem.Size = new System.Drawing.Size(56, 20);
-            this.menu2ToolStripMenuItem.Text = "Menu2";
+            this.menu2ToolStripMenuItem.Size = new System.Drawing.Size(111, 34);
+            this.menu2ToolStripMenuItem.Text = "Guardar ";
             // 
             // menu3ToolStripMenuItem
             // 
             this.menu3ToolStripMenuItem.Name = "menu3ToolStripMenuItem";
-            this.menu3ToolStripMenuItem.Size = new System.Drawing.Size(56, 20);
-            this.menu3ToolStripMenuItem.Text = "Menu3";
+            this.menu3ToolStripMenuItem.Size = new System.Drawing.Size(121, 34);
+            this.menu3ToolStripMenuItem.Text = "Proyectos";
             // 
             // menu4ToolStripMenuItem
             // 
             this.menu4ToolStripMenuItem.Name = "menu4ToolStripMenuItem";
-            this.menu4ToolStripMenuItem.Size = new System.Drawing.Size(71, 20);
+            this.menu4ToolStripMenuItem.Size = new System.Drawing.Size(123, 34);
             this.menu4ToolStripMenuItem.Text = "Acerca de";
             this.menu4ToolStripMenuItem.Click += new System.EventHandler(this.menu4ToolStripMenuItem_Click);
             // 
             // menu5ToolStripMenuItem
             // 
             this.menu5ToolStripMenuItem.Name = "menu5ToolStripMenuItem";
-            this.menu5ToolStripMenuItem.Size = new System.Drawing.Size(57, 20);
+            this.menu5ToolStripMenuItem.Size = new System.Drawing.Size(98, 34);
             this.menu5ToolStripMenuItem.Text = "AYUDA";
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Maroon;
-            this.ClientSize = new System.Drawing.Size(921, 421);
+            this.ClientSize = new System.Drawing.Size(1689, 777);
             this.Controls.Add(this.button6);
             this.Controls.Add(this.button5);
             this.Controls.Add(this.button4);
@@ -172,6 +180,7 @@ namespace Semana5_Grupo4_controles_de_formularios
             this.Controls.Add(this.button1);
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.Name = "Form1";
             this.Text = "Form1";
             this.menuStrip1.ResumeLayout(false);
