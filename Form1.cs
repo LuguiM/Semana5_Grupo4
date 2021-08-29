9c1b235ab9c357a0b3e366428ee2052bffdb7f7a
Angel <88108582+22denoviembre@users.noreply.github.com>
2021-08-29 10:22:36 -0600

    Update Form1.cs

--- Form1.cs
+++ Form1.cs
@@ -15,7 +15,7 @@ namespace Semana5_Grupo4_controles_de_formularios
         public Form1()
         {
             InitializeComponent();
-        }//prueba 2
+        }
 
         private void button3_Click(object sender, EventArgs e)
         {
